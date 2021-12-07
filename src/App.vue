856175c2b659f42eaa1078594beac7ac44df8c20
Jakub <jwolak@cryptovoucher.io>
2021-12-07 23:13:16 +0100

    inital menu design

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,66 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/Navigation.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div
+    class="background"
+  >
+    <div
+      class="application"
+    >
+      <Navigation
+        class="application__nav"
+      />
+    </div>
+  </div>
 </template>
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@400;500;600&family=Poppins:wght@100;200;300;400;500;600;700&display=swap');
+
+:root {
+  --bg-color: #262626;
+  --nav-bg: #FFFFFF;
+  --icon-color: var(--bg-color);
+  --text-color: var(--bg-color);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Baloo Bhaijaan 2', cursive, 'Poppins', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+}
+
+.background {
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  place-content: center;
+  align-items: center;
+}
+
+.application {
+  height: 736px;
+  width: 414px;
+  max-height: 90vh;
+  max-width: 90vw;
+
+  padding: 20px 7px !important;
+
+  display: flex;
+
+  background-color: var(--bg-color);
+
+  &__nav {
+    margin-top: auto !important;
+  }
 }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,145 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="navigation">
+    <ul class="navigation__list">
+      <li class="navigation__list__item navigation__list__item--active">
+        <a
+          href="#"
+          class="navigation__list__item__link"
+        >
+          <span class="navigation__list__item__icon">
+            <ion-icon name="home-outline" />
+          </span>
+          <span class="navigation__list__item__text">Home</span>
+        </a>
+      </li>
+      <li class="navigation__list__item">
+        <a
+          href="#"
+          class="navigation__list__item__link"
+        >
+          <span class="navigation__list__item__icon">
+            <ion-icon name="person-outline" />
+          </span>
+          <span class="navigation__list__item__text">Profile</span>
+        </a>
+      </li>
+      <li class="navigation__list__item">
+        <a
+          href="#"
+          class="navigation__list__item__link"
+        >
+          <span class="navigation__list__item__icon">
+            <ion-icon name="chatbubble-outline" />
+          </span>
+          <span class="navigation__list__item__text">Messages</span>
+        </a>
+      </li>
+      <li class="navigation__list__item">
+        <a
+          href="#"
+          class="navigation__list__item__link"
+        >
+          <span class="navigation__list__item__icon">
+            <ion-icon name="settings-outline" />
+          </span>
+          <span class="navigation__list__item__text">Settings</span>
+        </a>
+      </li>
+      <li class="navigation__list__item">
+        <a
+          href="#"
+          class="navigation__list__item__link"
+        >
+            <span class="navigation__list__item__icon">
+            <ion-icon name="camera-outline" />
+          </span>
+          <span class="navigation__list__item__text">Gallery</span>
+        </a>
+      </li>
+      <div
+        class="navigation__indicator"
+      ></div>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.navigation {
+  position: relative;
+  width: 100%;
+  height: 70px;
+  background-color: var(--nav-bg);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+
+  &__list {
+    width: 350px;
+    display: flex;
+
+    &__item {
+      position: relative;
+      list-style: none;
+      width: 70px;
+      height: 70px;
+      z-index: 1;
+
+      &--active &__icon {
+        transform: translateY(-35px);
+      }
+
+      &--active &__text {
+        opacity: 1;
+        transform: translateY(10px);
+      }
+
+      &__link {
+        position: relative;
+        display: flex;
+
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        width: 100%;
+
+        text-align: center;
+        font-weight: 500;
+      }
+
+      &__icon {
+        position: relative;
+        display: block;
+        line-height: 75px;
+        font-size: 1.5em;
+        text-align: center;
+        transition: .3s;
+        color: var(--icon-color);
+      }
+
+      &__text {
+        position: absolute;
+        color: var(--text-color);
+
+        font-weight: 400;
+        font-size: .75em;
+        letter-spacing: 0.05em;
+
+        transition: .3s;
+
+        opacity: 0;
+        transform: translateY(20px);
+      }
+    }
+  }
+}
+</style>
